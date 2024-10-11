4c18177e604741652b57c979b12737dbf551ecd3
Magnuscake <saneyar.k@gmail.com>
2024-10-11 13:56:03 +1100

    Remove nuxt/content and add nuxt/google-fonts

--- app.vue
+++ app.vue
@@ -3,3 +3,9 @@
     <NuxtPage />
   </div>
 </template>
+
+<style>
+body {
+  font-family: "Ubuntu Mono";
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ["@nuxt/content"],
+  modules: ["@nuxtjs/google-fonts"],
+  googleFonts: {
+    families: {
+      "Ubuntu Mono": true,
+    },
+  },
 })
