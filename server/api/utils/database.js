f0adef0f396b948fb2d17083a6be50930b545169
eigen-spaced <saneyar.k@gmail.com>
2024-10-14 16:39:05 +1100

    Move database to its own file

--- server/api/utils/database.js
+++ server/api/utils/database.js
@@ -1,11 +1,11 @@
 import pg from "pg"
 
 const pool = new pg.Pool({
-  host: "localhost",
-  port: 5432,
-  user: "postgres",
-  password: "fusrohdah",
-  database: "seemingly-random",
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  database: process.env.PG_DATABASE,
   max: 10,
 })
 
