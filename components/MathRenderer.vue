5500788e0e73946e3cf8d59a8c2595eb442cabad
eigen-spaced <saneyar.k@gmail.com>
2024-10-15 23:00:41 +1100

    Modify MathRenderer component to render block and inline equations

--- components/MathRenderer.vue
+++ components/MathRenderer.vue
@@ -1,25 +1,30 @@
+<script setup>
+import katex from "katex"
+
+const { formula, size } = defineProps({
+  formula: {
+    type: String,
+    required: true,
+  },
+  size: {
+    type: Number,
+    default: 16, // Default size in pixels
+  },
+})
+const renderedLatex = computed(() => {
+  // return katex.renderToString(this.formula, { throwOnError: false })
+  const parsedText = formula
+    .replace(/\$\$(.+?)\$\$/gs, (_, formula) =>
+      katex.renderToString(formula, { displayMode: true })
+    )
+    .replace(/\$(.+?)\$/g, (_, formula) =>
+      katex.renderToString(formula, { displayMode: false })
+    )
+
+  return parsedText
+})
+</script>
+
 <template>
   <div v-html="renderedLatex" :style="{ fontSize: size + 'px' }"></div>
 </template>
-
-<script>
-import katex from "katex"
-
-export default {
-  props: {
-    formula: {
-      type: String,
-      required: true,
-    },
-    size: {
-      type: Number,
-      default: 20, // Default size in pixels
-    },
-  },
-  computed: {
-    renderedLatex() {
-      return katex.renderToString(this.formula, { throwOnError: false })
-    },
-  },
-}
-</script>
