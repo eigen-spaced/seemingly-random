28640abaeae42097fbfa4dad204e576de36b978d
eigen-spaced <saneyar.k@gmail.com>
2024-10-15 17:51:23 +1100

    Stylistic changes to index page

--- pages/index.vue
+++ pages/index.vue
@@ -14,34 +14,49 @@ const entitiesCount = await useFetch("/api/entities/count").then((res) =>
 )
 
 rankColors.value = chroma
-  .scale(["#9290c4", "#d1daf9", "#89847F"])
+  .scale(["#ffcd21", "#FFA500", "#F08080"])
   .domain([0, entitiesCount - 1])
 </script>
 
 <template>
   <NuxtLayout>
-    <div class="rankings">
-      <ClientOnly fallback="Loading List...">
-        <div v-if="status === 'error'">
-          {{ error }}
-        </div>
-        <div
-          v-else
-          :style="{ background: rankColors(index) }"
-          class="ranked-item"
-          v-for="(item, index) in entities"
-          :key="index"
-        >
-          {{ item.entity }}
-        </div>
-      </ClientOnly>
-    </div>
+    <template #twocolumn>
+      <div class="description">
+        Explore how different items, people, and ideas stack up against each
+        other based on user choices. Each ranking reflects preferences collected
+        from head-to-head comparisons, giving you a dynamic view of what rises
+        to the top. The ranking system users a ML model under the hood to assign
+        a rank to an item based on how it performs against other items. Dive in,
+        make your picks, and see how your choices shape the results in
+        real-time!
+      </div>
+      <div class="rankings">
+        <ClientOnly fallback="Loading List...">
+          <div v-if="status === 'error'">
+            {{ error }}
+          </div>
+          <div
+            v-else
+            :style="{ background: rankColors(index) }"
+            class="ranked-item"
+            v-for="(item, index) in entities"
+            :key="index"
+          >
+            {{ item.entity }}
+          </div>
+        </ClientOnly>
+      </div>
+    </template>
   </NuxtLayout>
 </template>
 
-<style>
+<style scoped>
+.description {
+  font-size: 19px;
+}
+
 .rankings {
-  padding: 0 35rem;
+  padding: 0 1rem;
 }
 
 .ranked-item {
